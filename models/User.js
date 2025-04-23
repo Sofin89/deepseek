cc18d94d6757f9ebc6a7eaaf368380a4c0ccaee9
Sofin89 <sofinmansuri056@gmail.com>
2025-04-23 20:48:54 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    _id: {type: String, require: true},
+    name: {type: String, require: true},
+    email: {type: String, require: true},
+    image: {type: String, require: false}
+  },
+  {timestamps: true}
+);
+
+const User =mongoose.models.User || mongoose.model("User", UserSchema)   
+
+export default User;
+
